cc611936f76458806f0232b867b294248433e608
Ashley Stanton-Nurse <git@analogrelay.dev>
2026-05-06 07:56:49 -0700

    Service Implementation

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ struct Greeter {
     language: String,
 }
 
+impl Greeter {
+    fn format_greeting(&self) -> String {
+        match self.language.as_str() {
+            "es" => format!("¡Hola, {}!", self.user_name),
+            "fr" => format!("Bonjour, {}!", self.user_name),
+            _    => format!("Hello, {}!", self.user_name),
+        }
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
